689f6fafd99b749b225f81373f48e442643cb620
ThomUn <thomas.u1@gmx.at>
2017-10-16 22:50:41 +0200

    Add ES04_04_Pixelize

---
+++ ES04/ES04_04_Pixelize.m
@@ -0,0 +1,28 @@
+% Load jpeg files 
+clear all;
+
+%% Read image
+IM = rgb2gray(imread('castle.jpg'));
+%IM = rgb2gray(imread('ramsay.jpg'));
+
+%% Choose area to pixelize
+mask = roipoly(IM);
+
+%% Apply pixelize function to image
+IM_new = pixelize(IM, 5, mask);
+
+%% Display image
+figure(1);
+subplot(1,2,1);
+imshow(IM);
+title('Orignal image');
+
+subplot(1,2,2);
+imshow(IM_new);
+title('Pixelized image');
+
+function IM_new = pixelize(image, pixelsize, mask)
+    fun = @(x) uint8(ones(size(x))).*mean(x(:));
+    IM_new = blkproc(image, [pixelsize pixelsize], fun);
+    IM_new(~mask) = image(~mask);
+end

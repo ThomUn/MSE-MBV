487a609c8a458ae916eed84486622753cce28053
ThomUn <thomas.u1@gmx.at>
2017-10-25 01:25:00 +0200

    Add ES05_03_Thresholding styling

--- ES05/ES05_03_Thresholding.m
+++ ES05/ES05_03_Thresholding.m
@@ -1,18 +1,3 @@
-%Compare and evaluate the following thresholding methods:
-%a. Otsu (MATLAB built in)
-%b. Iterative optimal thresholding
-%c. A mode method
-%Compare and evaluate them for the images
-%a. Cameraman
-%b. Page
-%c. Ean
-%When evaluating, use a human-generated ground truth. That means manually threshold the images
-%yourself and compare the results of the automatic methods to your results.
-%For each image, create a neat figure that displays
-%• original image
-%• segmented images
-%• interesting stats (e.g. percentage object pixles, comparison to ground truth)
-%Include a conclusion.
 clear all;
 
 %% Read images
